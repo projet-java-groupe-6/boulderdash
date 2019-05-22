fef83e7228972411a537a03877320a2c72a0a349
Clément Vicart <clem.vicart@gmail.com>
2019-05-22 15:51:39 +0200

    cleaning code

--- main/src/main/java/main/Main.java
+++ main/src/main/java/main/Main.java
@@ -7,7 +7,7 @@ package main;
 import contract.ControllerOrder;
 import controller.Controller;
 import model.Model;
-import view.View;
+import view.BoardFrame;
 
 /**
  * The Class Main.
@@ -24,7 +24,7 @@ public abstract class Main {
      */
     public static void main(final String[] args) {
         final Model model = new Model();
-        final View view = new View(model);
+        final BoardFrame boardFrame = new BoardFrame("BoulderDash");
         final Controller controller = new Controller(view, model);
         view.setController(controller);
 
---
+++ model/src/main/java/model/DAOLevel.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.sql.Connection;
+
+/**
+ * The class DAOLevel
+ *
+ * @author clement
+ */
+public class DAOLevel {
+
+    /**  */
+    private static final int LEVEL = 1;
+
+    private Connection connection;
+
+    public DAOLevel(Connection connection) {
+
+    }
+}
