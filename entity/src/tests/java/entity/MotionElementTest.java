773971e0ce2741e980a8889ae81b7519e9a29d32
Parradizze <theo.boivin1@viacesi.fr>
2019-05-23 15:59:39 +0200

    Test character

--- entity/src/tests/java/entity/MotionElementTest.java
+++ entity/src/tests/java/entity/MotionElementTest.java
@@ -2,13 +2,28 @@ package entity;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MotionElementTest {
+	
+	private MotionElement character;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Before
+	public void setUp() {
+		this.character = new MotionElement("character.png");
+		this.character.setX(1);
+		this.character.setY(1);
 	}
-
+	
+	@Test
+	public void testGetX() {
+		int expected = 1;
+		assertEquals(expected, this.character.getX());
+	}
+	@Test
+	public void testGetY() {
+		int expected = 1;
+		assertEquals(expected, this.character.getY());
+	}
 }
