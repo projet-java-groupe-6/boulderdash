673bc86aeecb7e899877ce5ce4fd99a3f433d9d3
Gachita31 <44901156+Gachita31@users.noreply.github.com>
2019-05-23 16:01:59 +0200

    Tests

---
+++ model/src/tests/java/model/DAOModelTest.java
@@ -0,0 +1,49 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import java.awt.Point;
+import java.util.HashMap;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import entity.MotionElement;
+import entity.MotionlessElement;
+
+public class DAOModelTest {
+	
+	private DAOModel model;
+	private MotionElement character;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		this.model = new DAOModel();
+		this.character = new MotionElement("character.png");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetWalls() {
+		HashMap<Point, MotionlessElement> expected = new HashMap<>();
+		assertEquals(expected, this.model.getWalls());
+	}
+	public void testgetCharacter() {
+		assertEquals(character, this.model.getCharacter());
+	}
+
+}
