6c9ff08a31c875188364bbb763ff1303488d04b3
Gachita31 <44901156+Gachita31@users.noreply.github.com>
2019-05-24 09:59:51 +0200

    Keylistenner+Order

--- view/src/main/java/view/ViewFrame.java
+++ view/src/main/java/view/ViewFrame.java
@@ -40,55 +40,15 @@ public class ViewFrame extends JFrame implements KeyListener {
 
 	@Override
 	public void keyPressed(final KeyEvent e) {
-		int key = e.getKeyCode();
-
-		switch(e.getKeyCode())
-		{
-			case KeyEvent.VK_Z: 
-			case KeyEvent.VK_W:
-			case KeyEvent.VK_UP:
-			this.controller.orderPerform(Order.CHARACTER_UP);
-			break;
-			
-			
-			case KeyEvent.VK_S:
-			case KeyEvent.VK_DOWN:
-			this.controller.orderPerform(Order.CHARACTER_DOWN);
-			break;
-			
-			
-			case KeyEvent.VK_Q:
-			case KeyEvent.VK_LEFT:
-			this.controller.orderPerform(Order.CHARACTER_LEFT);
-			break;
-			
-			
-			case KeyEvent.VK_D:
-			case KeyEvent.VK_RIGHT:
-			this.controller.orderPerform(Order.CHARACTER_RIGHT);
-			break;
-			
-			case KeyEvent.VK_SPACE:
-			this.controller.orderPerform(Order.CHARACTER_SPELL);
-			break;
-
-			case KeyEvent.VK_R:
-				this.controller.orderPerform(Order.RETRY);
-				break;
-				
-		}
+		char key = e.getKeyChar();
+		Order order= new Order(key);
+		this.controller.orderPerform(order);
+		
 	}
 
 	@Override
 	public void keyReleased(final KeyEvent e) {
-    int key = e.getKeyCode();
-		
-		if( key == KeyEvent.VK_RIGHT) {
-			
-		}
-		if( key == KeyEvent.VK_LEFT) {
-			
-		}
+
 		
 	}
 
