42dcbb800ab8ca77f60e572d8d11b61b76e60e47
Gachita31 <44901156+Gachita31@users.noreply.github.com>
2019-05-24 09:07:35 +0200

    Keylistenner (a completer)

--- controller/src/main/java/controller/Controller.java
+++ controller/src/main/java/controller/Controller.java
@@ -11,7 +11,34 @@ public class Controller implements IController {
 
     @Override
     public void orderPerform(Order order) {
+		/*
+    	switch (order){
+		case CHARACTER_DOWN:
+			hm.move(Direction.DOWN);
+			break;
+		case CHARACTER_UP:
+			hm.move(Direction.UP);
+			break;
+		case CHARACTER_LEFT:
+			hm.move(Direction.LEFT);
+			break;
+		case CHARACTER_RIGHT:
+			hm.move(Direction.RIGHT);
+			break;
+		case CHARACTER_SPELL:
+			hm.sendSpell();
+			break;
+		case RETRY:
+			IHero h = model.getLevel().getHero();
+			h.setScore(h.getScore() - RETRY_COST);
+			this.model.loadLevel(this.model.getLevel().getId());
+			break;
+		default:
+			System.out.println("Not supported order : "+order.toString());
+			break;
+	}
 
+    */
     }
 
     @Override
--- view/src/main/java/view/ViewFrame.java
+++ view/src/main/java/view/ViewFrame.java
@@ -1,15 +1,24 @@
 package view;
 
+import contract.IController;
 import contract.IModel;
 import contract.IView;
+import contract.Order;
+import entity.MotionElement;
 
 import javax.swing.*;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Observable;
 import java.util.Observer;
 
-public class ViewFrame extends JFrame {
+public class ViewFrame extends JFrame implements KeyListener {
 
     private ViewPanel panel;
     private IView view;
+    private IController controller;
+    private Observable observable;
 
     public ViewFrame(IView v) {
         super("BoulderDash");
@@ -21,9 +30,71 @@ public class ViewFrame extends JFrame {
         this.panel = new ViewPanel(this.view.getModel());
         this.setContentPane(panel);
         this.setVisible(true);
+        this.addKeyListener(this);
+        observable.addObserver(panel);
     }
 
     public Observer getObserver() {
         return this.panel;
     }
+
+	@Override
+	public void keyPressed(final KeyEvent e) {
+		int key = e.getKeyCode();
+
+		switch(e.getKeyCode())
+		{
+			case KeyEvent.VK_Z: 
+			case KeyEvent.VK_W:
+			case KeyEvent.VK_UP:
+			this.controller.orderPerform(Order.CHARACTER_UP);
+			break;
+			
+			
+			case KeyEvent.VK_S:
+			case KeyEvent.VK_DOWN:
+			this.controller.orderPerform(Order.CHARACTER_DOWN);
+			break;
+			
+			
+			case KeyEvent.VK_Q:
+			case KeyEvent.VK_LEFT:
+			this.controller.orderPerform(Order.CHARACTER_LEFT);
+			break;
+			
+			
+			case KeyEvent.VK_D:
+			case KeyEvent.VK_RIGHT:
+			this.controller.orderPerform(Order.CHARACTER_RIGHT);
+			break;
+			
+			case KeyEvent.VK_SPACE:
+			this.controller.orderPerform(Order.CHARACTER_SPELL);
+			break;
+
+			case KeyEvent.VK_R:
+				this.controller.orderPerform(Order.RETRY);
+				break;
+				
+		}
+	}
+
+	@Override
+	public void keyReleased(final KeyEvent e) {
+    int key = e.getKeyCode();
+		
+		if( key == KeyEvent.VK_RIGHT) {
+			
+		}
+		if( key == KeyEvent.VK_LEFT) {
+			
+		}
+		
+	}
+
+	@Override
+	public void keyTyped(final KeyEvent e) {
+	    System.out.println("keyTyped");
+		
+	}
 }
