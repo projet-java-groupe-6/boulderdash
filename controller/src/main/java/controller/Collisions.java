06c6bc7202fe2ad3757656195eb884fd10218cd7
Clément Vicart <clem.vicart@gmail.com>
2019-05-27 09:10:17 +0200

    optimizing

--- controller/src/main/java/controller/Collisions.java
+++ controller/src/main/java/controller/Collisions.java
@@ -21,31 +21,27 @@ public class Collisions {
 		int x = element.getX();
 		int y = element.getY();
 		Point elementPoint = new Point(x, y);
-		Wall w = null;
-		Rock r = null;
 		switch (direction) {
 			case LEFT:
 				elementPoint.x -= 1;
-				w = this.model.getWalls().get(elementPoint);
 				break;
 			case UP:
 				elementPoint.y -= 1;
-				w = this.model.getWalls().get(elementPoint);
 				break;
 			case RIGHT:
 				elementPoint.x += 1;
-				w = this.model.getWalls().get(elementPoint);
 				break;
 			case DOWN:
 				elementPoint.y += 1;
-				w = this.model.getWalls().get(elementPoint);
 				break;
 		}
+		Wall wall = this.model.getWalls().get(elementPoint);
+		Rock r = null;
 		for(Rock rock: this.model.getRocks()) {
 			if(rock.getX() == elementPoint.x && rock.getY() == elementPoint.y) {
 				r = rock;
 			}
 		}
-		return w == null && r == null;
+		return wall == null && r == null;
 	}
 }
