fadd1a0c8c6884da29795824d4cb5b4e4d1f7dcb
Gachita31 <44901156+Gachita31@users.noreply.github.com>
2019-05-28 12:37:36 +0200

    Model tests

---
+++ model/src/tests/java/model/ScoreTest.java
@@ -0,0 +1,26 @@
+package model;
+
+import static org.junit.Assert.*;
+
+public class ScoreTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getNbrDiamonds() {
+    }
+
+    @org.junit.Test
+    public void setScore() {
+    }
+
+    @org.junit.Test
+    public void getScore() {
+    }
+}
