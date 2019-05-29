60bab7a5c91973e8d0b4735d29da4e8ab7974d11
Parradizze <theo.boivin1@viacesi.fr>
2019-05-29 12:06:25 +0200

    JavaDoc Entity

--- entity/src/main/java/entity/Level.java
+++ entity/src/main/java/entity/Level.java
@@ -7,14 +7,29 @@ import java.sql.Statement;
 
 public class Level {
 
+    /**
+     * Connection instance
+     */
     private Connection connection;
 
+    /**
+     * level map number 
+     */
     public static int levelNumber = 2;
 
+    /**
+     * The Constructor of Level
+     * @param connection
+     *      dataBased Connection
+     */
     public Level(Connection connection) {
         this.connection = connection;
     }
 
+    /**
+     * Method to get the path of the map
+     * @return the path of the map
+     */
     public String getPath() {
         Statement statement = null;
         try {
